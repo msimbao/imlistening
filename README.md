524c25b6a8de3e86a2368930ffa36b961e76d9d8
Glitch (hello-webpage) <none>
2021-04-06 18:11:37 +0000

    🎎🏋 Checkpoint ./style.css:633222/433 ./README.md:633222/235 ./index.html:633222/545 ./script.js:633222/2612

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 I'm Listening
 =================
 
-Cards with icebreakers
+Cards with icebreakers on them.
+
+Original Tinder Swiping code is from @developingidea on CodePen.
+
+Images From Open Doodles by Pablo Stanley.
+
+Huge thanks to both of them!
--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 $(document).ready(function(){
 
     $(".buddy").on("swiperight",function(){
-      $(this).addClass('rotate-left').delay(300).fadeOut(1);
-      $('.buddy').find('.status').remove();
-     
       if ( $(this).is(':last-child') ) {
-        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-       } else {
-          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-       }
+      alert("last Child Reached")
+      } else{
+        $(this).addClass('rotate-left').delay(300).fadeOut(1);
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(300);
+      }
+      
     });  
 
    $(".buddy").on("swipeleft",function(){
-    $(this).addClass('rotate-right').delay(300).fadeOut(1);
-    $('.buddy').find('.status').remove();
-
-    if ( $(this).is(':last-child') ) {
-     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-      alert('OUPS');
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-    } 
+      if ( $(this).is(':last-child') ) {
+      alert("last Child Reached")
+      } else{
+        $(this).addClass('rotate-right').delay(300).fadeOut(1);
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(300);
+      }
   });
 
 });
