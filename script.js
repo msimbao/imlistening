1ff940e8b47c80951f397bafb9801cb358c3c88f
Glitch (hello-webpage) <none>
2021-04-07 07:33:00 +0000

    🕰😎 Checkpoint ./index.html:633222/341 ./script.js:633222/7218

--- script.js
+++ script.js
@@ -39,106 +39,39 @@ firebase
     const data = snapshot.val();
     const questionsData = Object.values(data);
 
-  
-var app = new Vue({
-  el: "#app",
-  data: {
-    images: [], // Array To Hold Images
-    questions: [], // Array To Hold Questions
-    cards: [] // Array to hold card data which is are shuffled combinations of 7 words and 7 images
-  },
-  methods: {
-    shuffle: function() {
-      for (i = 0; i < 7; i++) {
-        var image = this.images[Math.floor(Math.random() * this.images.length)];
-        var question = this.questions[
-          Math.floor(Math.random() * this.questions.length)
-        ];
-        var obj = {
-          id: i,
-          image: image,
-          question: question
-        };
-        this.cards[i] = obj;
+    var app = new Vue({
+      el: "#app",
+      data: {
+        images: [], // Array To Hold Images
+        questions: [], // Array To Hold Questions
+        cards: [] // Array to hold card data which is are shuffled combinations of 7 words and 7 images
+      },
+      methods: {
+        shuffle: function() {
+          for (i = 0; i < 7; i++) {
+            var image = this.images[
+              Math.floor(Math.random() * this.images.length)
+            ];
+            var question = this.questions[
+              Math.floor(Math.random() * this.questions.length)
+            ];
+            var obj = {
+              id: i,
+              image: image,
+              question: question
+            };
+            this.cards[i] = obj;
+          }
+        }
+      },
+      created: function() {
+        this.images = images;
+        this.questions = questionsData;
+        this.shuffle();
       }
-    }
-  },
-  created: function() {
-    this.images = images;
-    this.questions = questionsData;
-    this.shuffle();
-
-  }
-});
+    });
   
-  
-$(document).ready(function() {
-  $(".buddy").on("swiperight", function() {
-    if ($(this).is(":last-child")) {
-      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-    } else if ($(this).is("#questionHolder")) {
-      var text = $("textarea#question").val();
-      firebase
-        .database()
-        .ref()
-        .push(text);
-      $(this)
-        .addClass("rotate-left")
-        .delay(300)
-        .fadeOut(1);
-      $(this)
-        .next()
-        .removeClass("rotate-left rotate-right")
-        .fadeIn(300);
-      } else {
-      $(this)
-        .addClass("rotate-left")
-        .delay(300)
-        .fadeOut(1);
-      $(this)
-        .next()
-        .removeClass("rotate-left rotate-right")
-        .fadeIn(300);
-    }
-  });
-
-  $(".buddy").on("swipeleft", function() {
-    if ($(this).is(":last-child")) {
-        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-    } else if ($(this).is("#questionHolder")) {
-      var text = $("textarea#question").val();
-      firebase
-        .database()
-        .ref()
-        .push(text);
-      $(this)
-        .addClass("rotate-left")
-        .delay(300)
-        .fadeOut(1);
-      $(this)
-        .next()
-        .removeClass("rotate-left rotate-right")
-        .fadeIn(300);
-      }else {
-      $(this)
-        .addClass("rotate-right")
-        .delay(300)
-        .fadeOut(1);
-      $(this)
-        .next()
-        .removeClass("rotate-left rotate-right")
-        .fadeIn(300);
-    }
-  });
-});
-
-  
-  });
-
-
-
-
-/**
+  /**
   * //////////////////////////////////////
  __      __         
    CARD SWIPE
@@ -148,10 +81,55 @@ $(document).ready(function() {
   * //////////////////////////////////////
   */
 
-/**
-  * //////////////////////////////////////
 
-    IMAGES URLS
-                    
-  * //////////////////////////////////////
-  */
+    $(document).ready(function() {
+      $(".buddy").on("swiperight", function() {
+        if ($(this).is(":last-child")) {
+          $(".buddy:nth-child(1)")
+            .removeClass("rotate-left rotate-right")
+            .fadeIn(300);
+        } else if ($(this).is("#questionHolder")) {
+          var text = $("textarea#question").val();
+          firebase
+            .database()
+            .ref()
+            .push(text);
+          $(this)
+            .addClass("rotate-left")
+            .delay(300)
+            .fadeOut(1);
+          $(this)
+            .next()
+            .removeClass("rotate-left rotate-right")
+            .fadeIn(300);
+        } else {
+          $(this)
+            .addClass("rotate-left")
+            .delay(300)
+            .fadeOut(1);
+          $(this)
+            .next()
+            .removeClass("rotate-left rotate-right")
+            .fadeIn(300);
+        }
+      });
+
+      $(".buddy").on("swipeleft", function() {
+        if ($(this).is(":last-child")) {
+          $(".buddy:nth-child(1)")
+            .removeClass("rotate-left rotate-right")
+            .fadeIn(300);
+        } else {
+          $(this)
+            .addClass("rotate-right")
+            .delay(300)
+            .fadeOut(1);
+          $(this)
+            .next()
+            .removeClass("rotate-left rotate-right")
+            .fadeIn(300);
+        }
+      });
+    });
+  });
+
