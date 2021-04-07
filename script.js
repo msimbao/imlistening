f5a39837d86db538846eaa26a281beb5e931ad09
Glitch (hello-webpage) <none>
2021-04-07 04:00:29 +0000

    ⚓️🌷 Checkpoint ./script.js:633222/7129

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
 /**
  *
  *  @Title script.js
  *
  *  @Brief JavaScript For holding functions that handle the client
  *
- *  @Author Mphatso 
+ *  @Author Mphatso
  *
  */
-
 
 /**
   * //////////////////////////////////////
@@ -18,14 +16,15 @@
   * //////////////////////////////////////
   */
 
-
-
-firebase.database().ref().once('value', (snapshot) => {
-  const data = snapshot.val();
-  const questionsData = Object.values(data);
-  console.log(questionsData)
-  app.questions = questionsData;
-});
+firebase
+  .database()
+  .ref()
+  .once("value", snapshot => {
+    const data = snapshot.val();
+    const questionsData = Object.values(data);
+    // console.log(questionsData);
+    app.questions = questionsData;
+  });
 /**
   * //////////////////////////////////////
 
@@ -34,53 +33,47 @@ firebase.database().ref().once('value', (snapshot) => {
   * //////////////////////////////////////
   */
 
-/** 
-* @name card-item
-* @brief Component For Shuffle-able card items
-*/
+/**
+ * @name card-item
+ * @brief Component For Shuffle-able card items
+ */
 
-       <div class="buddy">
-          <div class="avatar" style="display: block;">
-            <h1>
-              Thank You!
-            </h1>
-            <h2>
-              Your contribution will enrich the lives of other players!
-            </h2>
-          </div>
-        </div>
-
-Vue.component("card", {
+Vue.component("card-item", {
   props: ["card"],
   template:
-    '<div class="buddy"'+
-    '<div class="avatar" style="display: block;">'+
-    ''
-    '<h3>{{card.words}}</h3>'
-    '<a :href="feed.url" target="_blank" rel="noopener noreferrer">' +
-    '<transition name="fade">' +
-    '<div class="newsItem card">' +
-    '<div class="newsImageHolder">' +
+    '<div class="buddy">' +
+    '<div class="avatar" style="display: block;">' +
     '<img :src="card.image" >' +
+    "<h3>{{card.words}}</h3>" +
     "</div>" +
-    '<div class="articleWords">' +
-    "<h4>{{feed.title}}</h4>" +
-    '<p>"{{feed.description}}..."</p>' +
-    "<h6>{{feed.author}}</h6>" +
-    "</div>" +
-    '<img class="teamImage" :src="feed.teamLogo">' +
-    "</div>" +
-    "</transition>" +
-    "</a>",
+    "</div>"
 });
 
 var app = new Vue({
   el: "#app",
   data: {
-    images: images,  // Array To Hold Images
-    questions: [] // Array To Hold Questions
-  }
-})
+    images: images, // Array To Hold Images
+    questions: [], // Array To Hold Questions
+    entries: [] // Array to hold card data which is are shuffled combinations of 7 words and 7 images
+  },
+  methods: {
+    shuffle: function() {
+      for (i = 0; i < 7; i++) {
+        var imageItem = this.images[Math.floor(Math.random() * this.images.length)];
+        var wordsItem = this.questions[Math.floor(Math.random() * this.questions.length)];
+        var obj = {
+          image: imageItem,
+          words: wordsItem
+        };
+        this.entries[i] = obj;
+      }
+    }
+  },
+   created: function () {
+     this.shuffle()
+   }
+  
+});
 
 /**
   * //////////////////////////////////////
@@ -92,39 +85,51 @@ var app = new Vue({
   * //////////////////////////////////////
   */
 
+$(document).ready(function() {
+  $();
 
-$(document).ready(function(){
-  
-    $()
-
-    $(".buddy").on("swiperight",function(){
-      if ( $(this).is(':last-child') ) {
-      alert("This is the Last card :(")
-      }
-      else if( $(this).is('#questionHolder') ) {
-        var text = $('textarea#question').val();
-        firebase.database().ref().push(text)
-        // $(this).addClass('rotate-left').delay(300).fadeOut(1);
-        // $(this).next().removeClass('rotate-left rotate-right').fadeIn(300);
-      }
-      else{
-        $(this).addClass('rotate-left').delay(300).fadeOut(1);
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(300);
-      }
-      
-    });  
-
-   $(".buddy").on("swipeleft",function(){
-      if ( $(this).is(':last-child') ) {
-      alert("last Child Reached")
-      } else{
-        $(this).addClass('rotate-right').delay(300).fadeOut(1);
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(300);
-      }
+  $(".buddy").on("swiperight", function() {
+    if ($(this).is(":last-child")) {
+      alert("This is the Last card :(");
+    }
+    else if ($(this).is(":first-child")) {
+      app.shuffle()
+    }
+    else if ($(this).is("#questionHolder")) {
+      var text = $("textarea#question").val();
+      firebase
+        .database()
+        .ref()
+        .push(text);
+      // $(this).addClass('rotate-left').delay(300).fadeOut(1);
+      // $(this).next().removeClass('rotate-left rotate-right').fadeIn(300);
+    } else {
+      $(this)
+        .addClass("rotate-left")
+        .delay(300)
+        .fadeOut(1);
+      $(this)
+        .next()
+        .removeClass("rotate-left rotate-right")
+        .fadeIn(300);
+    }
   });
 
+  $(".buddy").on("swipeleft", function() {
+    if ($(this).is(":last-child")) {
+      alert("last Child Reached");
+    } else {
+      $(this)
+        .addClass("rotate-right")
+        .delay(300)
+        .fadeOut(1);
+      $(this)
+        .next()
+        .removeClass("rotate-left rotate-right")
+        .fadeIn(300);
+    }
+  });
 });
-
 
 /**
   * //////////////////////////////////////
